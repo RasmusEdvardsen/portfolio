07fbc932248bd06c030882aea2fea7e81b0415d9
Rasmus Edvardsen <Rasmus.Edvardsen@infomedia.dk>
2020-04-05 20:05:51 +0200

    Add router, clean up scaffold code

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="home">
+        Hello from home!
+    </div>
+</template>
+
+<script>
+import Vue from "vue"
+import { Component } from 'vue-property-decorator';
+
+@Component
+export default class Home extends Vue {
+
+}
+</script>
+
+<style>
+
+</style>
